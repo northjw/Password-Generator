71a58045eff6b6775a27eafe65ecd731058fa3df
mini pony <northjw1@gmail.com>
2020-10-13 13:39:00 -0400

    addding characyters to be added in password

--- script.js
+++ script.js
@@ -12,3 +12,128 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// ======= vars definition ======
+// Array of special characters to be included in password
+var specialCharacters = [
+
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  "."
+];
+// Array of UPPERCASE characters to be included in password
+
+var upperCaseCharacters = [
+
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+
+];
+// Array of UPPERCASE characters to be included in password
+
+var lowerCaseCharacters = [
+
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+
+];
+
+// Array of numeric characters to be included in password
+
+var lowerCaseCharacters = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+
+];
+var totalCharacters = [];
+
+var confirmUppercase = " ";
+var confirmLowercase = " ";
+var confirmNumbers = " ";
+var confirmSpecial = " ";
+var characterCount = " ";
+
+
+
+// ==============  function definitions  ==============  
+function getUserOptions() {
+
+
+
+}
